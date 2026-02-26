cefc67dc1a747fdff332abc2536c847e8279d395
Mick Enev <menev2@illinois.edu>
2026-02-25 18:05:03 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,20 @@
 #include <string>
 
 int main() {
-
-  std::string input = "";
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
 
-  std::cin >> input;
-
-  if (input.size() > 0) {
-    std::cout << input << ": " << "command not found" << std::endl;
+  while (true) {
+    std::string input = "";
+    std::cout << "$ ";
+  
+    std::cin >> input;
+  
+    if (input.size() > 0) {
+      std::cout << input << ": " << "command not found" << std::endl;
+    }
   }
+
 }

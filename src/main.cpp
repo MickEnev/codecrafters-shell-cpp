f72ef3ebd789f783bff0bc7d9f5cd9c289a0ab2d
Mick Enev <menev2@illinois.edu>
2026-02-25 20:50:26 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  fs::path curDir = fs::current_path();
 
   std::vector<std::string> validCommands = {"exit", "echo", "type"};
 
@@ -34,6 +39,10 @@ int main() {
         if (std::find(validCommands.begin(), validCommands.end(), command) != validCommands.end()) {
           std::cout << command << " is a shell builtin" << std::endl;
         } else {
+          const char* path_env = std::getenv("PATH");
+            for (int i = 0; i < std::strlen(path_env); i++) {
+              std::cout << path_env[i] << std::endl;
+            }
           std::cout << command << ": " << "not found" << std::endl;
         }
       }

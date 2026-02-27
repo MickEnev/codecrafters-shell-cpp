13efdc1c69405158b9358a8f2a0daecd121f3c1b
Mick Enev <menev2@illinois.edu>
2026-02-27 16:49:14 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <sstream>
 #include <unistd.h> 
+#include <cstring>
 
 namespace fs = std::filesystem;
 
@@ -26,7 +27,47 @@ int main() {
     std::cin >> input;
   
     if (std::find(validCommands.begin(), validCommands.end(), input) == validCommands.end()) {
-      std::cout << input << ": " << "command not found" << std::endl;
+      std::string input;
+      std::getline(std::cin, input);
+
+      std::stringstream iss(input);
+      std::vector<std::string> args;
+      std::string temp;
+
+      while (iss >> temp) {
+          args.push_back(temp);
+      }
+      std::vector<char*> argv;
+
+      for (auto& s : args) {
+          argv.push_back(const_cast<char*>(s.c_str()));
+      }
+      argv.push_back(nullptr);
+      // custom stuffs
+      const char* path_env = std::getenv("PATH");
+          // parse each directory 
+          std::vector<std::string> parts;
+          std::stringstream ss(path_env);
+          std::string token;
+
+          while (std::getline(ss, token, ':')) {
+              parts.push_back(token);
+          }
+
+          std::string command = argv[0];
+          std::string arg1 = argv[1];
+          std::string arg2 = argv[2];
+
+          for (const auto& p : parts) {
+            std::string file = p + "/" + command;
+            if (fs::exists(file) && access(file.c_str(), X_OK) == 0) {
+                execv(file.c_str(), argv.data());
+            } else {
+              std::cout << input << ": " << "command not found" << std::endl;
+            }
+          }
+
+
     } else {
       if (input == "exit") {
         break;
